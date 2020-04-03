82219d9af7e51e4c37036b4de7bab4f0d0eac867
Miguel Angel <miguela_nunez@outlook.com>
2020-04-02 20:31:59 -0600

    Body parser y Post

--- index.js
+++ index.js
@@ -1,50 +1,56 @@
 // Import things (Libraries)
 const express = require("express");
+const bodyparser = require("body-parser");
 // Import DB
 const { pokemon } = require("./pokedex.json");
+
 // Instancia de la clase express
 const app = express();
 
+// Metodos y componentes que utiliza la aplicación de Bodyparser.
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// POST
+// POST usa un body que es un atributo de la petición.
+app.post("/pokemon/", (req, res, next) => {
+    return res.status(200).send("Estas en pokemon POST, este es el id:" + req.body.name);
+});
+
 //Ruta default
 app.get("/", (req, res, next) => {
-  res.status(200);
-  //   const pokemon = pokedex.pokemon;
-  res.send("Bienvenido al Pokedex");
+    //   const pokemon = pokedex.pokemon;
+    return res.status(200).send("Bienvenido al Pokedex");
 });
-
 // Consulta por ID
 app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
-  const id = req.params.id - 1;
-  if (id >= 0 && id <= 150) {
-    res.status(200);
-    return res.send(pokemon[id]);
-  }
-  res.status(404);
-  res.send("Pokemon no encontrado");
-});
-
-// Consulta por nombre.
-app.get("/pokemon/:name", (req, res, next) => {
-  const name = req.params.name;
-  for (let i = 0; i < pokemon.length; i++) {
-    if (pokemon[i].name == name) {
-      res.status(200);
-      res.send(pokemon[i]);
+    const id = req.params.id - 1;
+    if (id >= 0 && id <= 150) {
+        return res.status(200).send(pokemon[id]);
     }
-  }
-  res.status(404);
-  res.send("Pokemon no encontrado");
+    return res.status(404).send("Pokemon no encontrado");
 });
 
 // Ruta de pokemon
-app.get("/pokemon/all", (req, res, next) => {
-  res.status(200);
-  res.send(pokemon);
+app.get("/pokemon", (req, res, next) => {
+    return res.status(200).send(pokemon);
+});
+
+// Consulta por nombre.
+app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) => {
+    const name = req.params.name;
+    const pk = pokemon.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase()) && p;
+    });
+    if (pk.length > 0) {
+        res.status(200).send(pk);
+    }
+    return res.status(404).send("Pokemon no encontrado");
 });
 
 // process.env.PORT => Define the default port to run and if it isn´t available uses the port 3000
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000");
 });
 
 // ******* Ejemplos *******
